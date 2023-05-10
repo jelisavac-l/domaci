e522a6d4a436c6a9c969a56abcdf6b71f822dade
jelisavac-l <jelisavacluka63@gmail.com>
2023-05-10 20:28:15 +0200

    Pocetak

--- Principi programiranja/Seminarski/main.c
+++ Principi programiranja/Seminarski/main.c
@@ -1,18 +1,38 @@
-#include "SDL2/SDL.h" 
+#include "SDL2/SDL.h"
 #include <stdio.h>
+#include <stdbool.h>
 
-int main( int argc, char* args[] ) 
-{ 
-  SDL_Window     *window;
+int main(int argc, char *args[])
+{
+  // Pokazivac na prozor
+  SDL_Window *window;
 
-  SDL_Init(SDL_INIT_VIDEO);
-  window = SDL_CreateWindow("SDL2 Window", 100, 100, 640, 480, 0); 
-  if(window == NULL)
+  SDL_Init(SDL_INIT_EVERYTHING);
+  window = SDL_CreateWindow("Geo554", 100, 100, 640, 480, SDL_WINDOW_SHOWN);
+  SDL_Surface *screen = SDL_GetWindowSurface(window);
+  if (window == NULL)
   {
-  	printf("Nije moguce otvoriti novi prozor: %s\n", SDL_GetError());
-  	return 1;
+    printf("Nije moguce otvoriti novi prozor: %s\n", SDL_GetError());
+    return 1;
   }
-  SDL_Delay(3000);
+
+  // Glavni loop
+  bool run = true;
+  while (run)
+  {
+    SDL_Event ev;
+    while (SDL_PollEvent(&ev))
+    {
+      switch (ev.type)
+      {
+      case SDL_QUIT:
+        {
+        run = false;
+        break;
+        }
+      }
+    }
+  }
   SDL_DestroyWindow(window);
   SDL_Quit();
   return 0;
